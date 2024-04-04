6c02b5138434f704dd5688f984b88637c2c373e2
Manoel Bruno <150696467+Bruhnodev17@users.noreply.github.com>
2024-04-04 13:58:13 -0300

    "<feat>:<Criação de model>"

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Produto extends Model
 {
     use HasFactory;
-}
+    protected $table = "produto";
+    protected $fillable = [
+        'name',
+        'price',
+        'validity',
+        'perishable',
+        'stock',];
+};
